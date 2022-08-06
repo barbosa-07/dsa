39c3f5662a894729ba0d004a2fca6ea3d13c3579
Rizwan Akram <rizwanakram4049@gmail.com>
2022-08-06 09:48:23 +0530

    feat: formatting fit

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,18 +1,18 @@
 function selectionSort(nums) {
-    for (let i = 0; i < nums.length; i++) {
-      let minIndex = i;
-      for (let j = i; j < nums.length; j++) {
-        if (nums[j] < nums[minIndex]) {
-          minIndex = j
-        }
+  for (let i = 0; i < nums.length; i++) {
+    let minIndex = i;
+    for (let j = i; j < nums.length; j++) {
+      if (nums[j] < nums[minIndex]) {
+        minIndex = j;
       }
-      // swap
-      let temp = nums[minIndex]
-      nums[minIndex] = nums[i]
-      nums[i] = temp
     }
-  
-    return nums
+    // swap
+    let temp = nums[minIndex];
+    nums[minIndex] = nums[i];
+    nums[i] = temp;
   }
-  
-  console.log(selectionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]))
+
+  return nums;
+}
+
+console.log(selectionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
