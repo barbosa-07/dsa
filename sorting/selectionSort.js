b91d3feea62780e98dd4b53c4e4ad62c19658f00
Rizwan Akram <rizwanakram4049@gmail.com>
2022-08-06 09:30:58 +0530

    feat: selection sort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(nums) {
+    for (let i = 0; i < nums.length; i++) {
+      let minIndex = i;
+      for (let j = i; j < nums.length; j++) {
+        if (nums[j] < nums[minIndex]) {
+          minIndex = j
+        }
+      }
+      // swap
+      let temp = nums[minIndex]
+      nums[minIndex] = nums[i]
+      nums[i] = temp
+    }
+  
+    return nums
+  }
+  
+  console.log(selectionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]))
